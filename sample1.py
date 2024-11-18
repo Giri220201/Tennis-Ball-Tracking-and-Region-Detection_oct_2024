a070b3163113f81c96347fa5f86cda870338d217
Giri220201 <giridet2001@gmail.com>
2024-11-18 19:20:14 +0530

    Update sample1.py
    Signed-off-by: Giri220201 <giridet2001@gmail.com>

--- sample1.py
+++ sample1.py
@@ -28,8 +28,10 @@ st.markdown(
         background-size: cover;
         background-position: center;
     }
-    h1, h2, h3, h4, h5, h6, p, label, .stMarkdown, .stTextInput>div>div>div>div>div, .stDataFrame {
-        color: white !important; /* Force all text elements to be white */
+     .title {
+        color: white;
+        font-size: 36px;
+        font-weight: bold;
     }
 
     .stButton>button {
@@ -52,7 +54,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.title("🎾 Tennis Game Tracking")
+st.markdown('<h1 class="title">🎾 Tennis Game Tracking</h1>', unsafe_allow_html=True)
 # Create two columns for layout
 col1, col2 = st.columns([3, 1])
 
